a5b3baad49645cca14946bcf75aa6ec5aacbe97f
Gabriel Pedrão <37489604+GSPedrao@users.noreply.github.com>
2021-07-27 09:16:51 -0300

    Mudanças no tabela.js

--- tabela.js
+++ tabela.js
@@ -3,19 +3,24 @@ var dadosLista = [];
 function salvarUser() {
 let nomeUser = document.getElementById("nomeUser").value;
 
-    if(nomeUser){
+
+    if(nomeUser && numero && pagamento){
     dadosLista.push(nomeUser);
+    dadosLista.push(numero);
+    dadosLista.push(pagamento);
     criaLista();
     document.getElementById("nomeUser").value = "";
+    document.getElementById("numero").value = "";
+    document.getElementById("pagamento").value = "";
     } else {
         alert("Usuário favor preencher o campo nome");
     }
 }
 
 function criaLista() {
-    let tabela = document.getElementById("tabela").innerHTML = "<tr><th>Nome</th><th></th></tr>";
+    let tabela = document.getElementById("tabela").innerHTML = "<tr><th>Nome</th></tr>";
     for(let i = 0; i <= (dadosLista.length -1); i++) {
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button> <button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
+        tabela += "<tr><td><th><th>" + dadosLista[i] + "</td><td><button class='btn btn-success' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button> <button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
         document.getElementById("tabela").innerHTML = tabela;
     }
 }

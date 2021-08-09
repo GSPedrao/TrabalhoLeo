1574086cfe09251136d1e8b096294ef589bcef5d
Gabriel Pedrão <gabrielsouzap@hotmail.com.br>
2021-08-09 20:29:43 -0300

    Arruma numero da tabela

--- tabela/tabela.js
+++ tabela/tabela.js
@@ -1,19 +1,21 @@
 var dadosUser = []
 var dadosNumero = []
 var dadosPagamento = []
-var valores 
+var valores = 0 
+
 
 function salvarUser() {
+
     let nomeUser = document.getElementById("nomeUser").value;
     let numero = document.getElementById("numero").value;
     let pagamento = document.getElementById("pagamento").value;
-
 
     if (nomeUser && numero && pagamento) {
         dadosUser.push(nomeUser);
         dadosNumero.push(numero);
         dadosPagamento.push(pagamento);
         criaLista();
+        //Limpa dados do formulario
         document.getElementById("nomeUser").value = "";
         document.getElementById("numero").value = "";
         document.getElementById("pagamento").value = "";
@@ -28,6 +30,7 @@ function criaLista() {
         tabela += "<tr><td>" + dadosUser[valores] + "</td><td>" + dadosNumero[valores] + "</td><td>" + dadosPagamento[valores] + "</td><td><button class='btn btn-success' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button> <button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
         document.getElementById("tabela").innerHTML = tabela;
     }
+   //console.log(tabela);
 }
 
 function excluir(valores) {
@@ -38,10 +41,16 @@ function excluir(valores) {
 }
 
 function editar(valores) {
-    document.getElementById("nomeUser").value = dadosUser[(valores - 1)];
-    document.getElementById("numero").value = dadosNumero[(valores - 1)];
-    document.getElementById("pagamento").value = dadosPagamento[(valores - 1)];
-    dadosUser.splice(dadosUser[(valores - 1)], 1);
-    dadosNumero.splice(dadosNumero[valores - 1], 1);
-    dadosPagamento.splice(dadosPagamento[valores - 1], 1);
+
+    var posicaoE = (valores -1);
+    document.getElementById("nomeUser").value = dadosUser[(posicaoE)];
+    document.getElementById("numero").value = dadosNumero[(posicaoE)];
+    document.getElementById("pagamento").value = dadosPagamento[(posicaoE)];
+
+    //pelo que entendi o splice nao precisa substituir no array, entao em segundo plano ele deleta o arquivo e depois na função salvar ele insere um novo valor no array
+    //até pensei em colcoar mais uma função de atualização, mas para o que vc precisa acho que ja serve. 
+    dadosUser.splice(posicaoE, 1);
+    dadosNumero.splice(posicaoE, 1);
+    dadosPagamento.splice(posicaoE, 1);
+
 }
